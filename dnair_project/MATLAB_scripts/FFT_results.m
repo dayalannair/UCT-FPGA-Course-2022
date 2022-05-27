2df9ec7732e448fe6de39512687ba8dca7dc10c3
Dayalan <nairdayalan@gmail.com>
2022-05-27 08:08:37 +0200

    updated gitignore

---
+++ dnair_project/MATLAB_scripts/FFT_results.m
@@ -0,0 +1,17 @@
+format longg
+Re_tab = readtable('FFT_out_Re.txt','Delimiter' ,'\n', 'BinaryType','int32');
+Im_tab = readtable('FFT_out_Im.txt','Delimiter' ,'\n', 'BinaryType','int32');
+%%
+
+Re = table2array(Re_tab);
+Im = table2array(Im_tab);
+
+FFT_mag = sqrt(Re.^2 + Im.^2);
+
+%%
+fs = 200e3;
+f=fs/2*[-1:2/512:1-2/512];
+close all
+figure
+%plot(FFT_mag(1:end-1));
+plot(FFT_mag)
