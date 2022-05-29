9a28b53e0d236f6b695dc4f8152e7ca589c89198
Dayalan <nairdayalan@gmail.com>
2022-05-29 18:56:56 +0200

    MATLAB code for example wave coe file

--- dnair_project/MATLAB_scripts/wave2coe.m
+++ dnair_project/MATLAB_scripts/wave2coe.m
@@ -1,6 +1,23 @@
-I = readtable('I.txt','Delimiter' ,' ');
-Q = readtable('Q.txt','Delimiter' ,' ');
 
+%% Generate chirp wave
+fs = 200e3;
+t_sweep = 1e-3;
+f0 = 0;
+f1 = 240e2;
+t = 0:1/fs:(t_sweep-1/fs);
+y = chirp(t,f0,t_sweep,f1);
+Y = fft(y);
+f = f_ax(200, fs);
+%% Plot
+close all
+figure
+tiledlayout(2,1)
+nexttile
+plot(t,y)
+nexttile
+plot(f, fftshift(abs(Y)))
+
+%%
 fid_I = fopen('I.coe', 'w');
 fid_Q = fopen('Q.coe', 'w');
 
@@ -15,24 +32,6 @@ fprintf(fid_I, "%x,\n", I_row(1:end)');
 
 Q_row = table2array(Q(1,1:200));
 fprintf(fid_Q, "%x,\n", Q_row(1:end)');
-%% Generate chirp wave
-fs = 200e3;
-t_sweep = 1e-3;
-f0 = 0;
-f1 = 240e2;
-t = 0:1/fs:(t_sweep-1/fs);
-y = chirp(t,f0,t_sweep,f1);
-Y = fft(y);
-f = f_ax(200, fs);
-close all
-figure
-tiledlayout(2,1)
-nexttile
-plot(t,y)
-nexttile
-plot(f, fftshift(abs(Y)))
-
-
 %%
 
 % zero padding
